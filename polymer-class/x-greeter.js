478bb30fc3348878d191abe57e5999a8f16c2b33
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2016-04-13 12:05:59 -0500

    fixed polymer-class example

--- polymer-class/x-greeter.js
+++ polymer-class/x-greeter.js
@@ -1,9 +1,14 @@
+'use strict';
+
 class XGreeter extends HTMLElement {
   createdCallback() {
     console.log('x-greeter.js createdCallback: entered');
-    this.createShadowRoot().innerHTML = 'test';
+    const name = this.getAttribute('name');
+    this.createShadowRoot().innerHTML =
+      `<h1>Hello, ${name}!</h1>
+       <p>This is a second line of content.</p>`;
   }
 }
 
-document.createElement('x-greeter', XGreeter);
+document.registerElement('x-greeter', XGreeter);
 console.log('x-greeter.js: created element');
