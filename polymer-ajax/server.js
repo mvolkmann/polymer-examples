a50edd2f9d7652c5451b7bde317cb4ca8c332ba2
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2016-04-15 16:37:38 -0500

    added polymer-ajax example

---
+++ polymer-ajax/server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+app.use(bodyParser.json()); // handle Content-Type 'application/json' requests
+app.use(bodyParser.text()); // handle Content-Type 'text/plain' requests
+app.use(express.static(__dirname));
+
+const names = ['Mark', 'Tami', 'Amanda', 'Jeremy'];
+let index = 0;
+app.get('/name', (req, res) => {
+  const prefix = req.query.prefix;
+  const obj = {name: prefix + ' ' + names[index]};
+
+  // Move index to the next name.
+  index++;
+  if (index === names.length) index = 0;
+
+  res.send(obj);
+});
+
+const PORT = 8081;
+app.listen(PORT, () => console.log('browse http://localhost:' + PORT));
